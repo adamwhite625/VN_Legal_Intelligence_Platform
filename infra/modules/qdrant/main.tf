afe0e75799db945045f113c776912a4df70bed31
thiennh <nguyenhaithien2k5@gmail.com>
2026-07-29 15:09:33 +0700

    fix(terraform): fix authorization_config block name in qdrant module

--- infra/modules/qdrant/main.tf
+++ infra/modules/qdrant/main.tf
@@ -59,7 +59,7 @@ resource "aws_ecs_task_definition" "qdrant" {
     efs_volume_configuration {
       file_system_id     = aws_efs_file_system.qdrant.id
       transit_encryption = "ENABLED"
-      authorization_configuration {
+      authorization_config {
         access_point_id = aws_efs_access_point.qdrant.id
         iam             = "DISABLED"
       }
